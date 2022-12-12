eb738ad2b35d2473bf357fe2d6f0dd751397a081
Arsenii <musatov@inbox.ru>
2022-12-12 16:46:24 +0700

    addNewApp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log(1);
+let numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: {
+
+    },
+    privat: false
+}
+
+let lastMovie = prompt ('Один из последних просмотренных фильмов?', '');
+let rateMovie = prompt ('На сколько оцените его', '');
+
+personalMovieDB.movies[lastMovie] = rateMovie;
+
+lastMovie = prompt ('Один из последних просмотренных фильмов?', '');
+rateMovie = prompt ('На сколько оцените его', '');
+
+personalMovieDB.movies[lastMovie] = rateMovie;
+console.log(personalMovieDB);
