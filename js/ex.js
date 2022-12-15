63a764fc91925f3225a1945e07fa24c539e2461d
Arsenii <musatov@inbox.ru>
2022-12-15 22:18:36 +0700

    addNewEx

--- js/ex.js
+++ js/ex.js
@@ -126,3 +126,87 @@
 // return result;
 // }
 // console.log(getMathResult(3,10));
+
+
+// ex 7
+
+// function calculateVolumeAndArea(value) {
+//     if (typeof(value) == 'number' && value > 0 && Number.isInteger(value)){
+//         let S = value * value * 6;
+//         let V = value * value * value;
+//         return `Объем куба: ${V}, площадь всей поверхности: ${S}`;
+//     } else{
+//         return (`При вычислении произошла ошибка`);
+//     }
+// }
+// console.log(calculateVolumeAndArea(5));
+
+// function getCoupeNumber(seat) {
+//     if (typeof(seat) == 'number' && Number.isInteger(seat) && seat > 0 && seat < 37){
+//         return Math.ceil(seat / 4);
+//     } else if (typeof(seat) == 'number' && Number.isInteger(seat) && seat == 0 || seat > 36){
+//         return 'Таких мест в вагоне не существует';
+//     } else {
+//         return 'Ошибка. Проверьте правильность введенного номера места';
+//     }
+// }
+// console.log(getCoupeNumber(37));
+
+// ex8
+
+// function getTimeFromMinutes(time) {
+//     if (typeof(time) == 'number' && time >= 0 && Number.isInteger(time)){
+
+//         let hour = Math.floor(time / 60) +'';
+//         let minute = time % 60 + ' минут';
+
+//         if (hour.length >= 1 && hour.slice(-1) == 1 && hour.slice(-2) != 11){
+//             hour += ' час';
+//         } else if (hour.slice(-1) >= 2 && hour.slice(-1) <= 4){
+//             hour += ' часа';
+//         } else {
+//             hour += ' часов';
+//         }
+
+//         return `Это ${hour} и ${minute}`;
+
+//     } else {
+//         return 'Ошибка, проверьте данные';
+//     }
+
+// }
+// console.log(getTimeFromMinutes(1846));
+
+// function findMaxNumber(a, b, c, d) {
+//     let comp = a + b + c + d;
+//     if (typeof(comp) == 'number' && !isNaN(comp)){
+//         return Math.max(a, b, c, d);
+//     } else {
+//         return 0;
+//     }
+
+// }
+// console.log(findMaxNumber(-1, 18.5, 13.3, 56));
+
+// function fib(value) {
+//     let c;
+//     let a = 0;
+//     let b = 1;
+//     let result = '0';
+//     if (typeof(value) == 'number' && Number.isInteger(value) && value > 1){
+//         result += ' 1';
+//         for (let i = 2; i < value; i++){
+//             c = a + b;
+//             result += ` ${c}`;
+//             a = b;
+//             b = c;
+//         }
+//         return result;
+//     } else if (value == 1){
+//         return result;
+//     } else {
+//         result = '';
+//         return result;
+//     }
+// }
+// console.log(fib(10));
